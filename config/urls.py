f1826339f64ed18213a4a8bda6c5953979f70744
carlos47fnaf <kaduardomello@gmail.com>
2023-07-28 14:06:37 -0300

    first commit

---
+++ config/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+
+from usuario.router import router as usuario_router
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
+router.register(r"livros", LivroViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(usuario_router.urls))
+]
